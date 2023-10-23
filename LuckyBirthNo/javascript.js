aa8b2f1f6b75f9229e536aee29b6a18166557730
mgmanoj2018 <mgmanoj2018@gmail.com>
2023-10-23 11:41:44 +0530

    add new

--- LuckyBirthNo/javascript.js
+++ LuckyBirthNo/javascript.js
@@ -1,1 +1,33 @@
-console.log("hello")
+console.log("hello");
+const dob = document.querySelector("#Date-birth");
+const luckyNO= document.querySelector("#Lucky-no");
+const clickBtn = document.querySelector("#check");
+const outPutdiv = document.querySelector("#outPut")
+
+function calculationNo(){
+    const stringDob = dob.value;
+    let sum = stringDob.replaceAll("-","");
+    let sumOfdov = 0;
+    for(let i=0; i<sum.length; i++){
+        sumOfdov = sumOfdov + Number(sum.charAt(i));
+    }
+    console.log(sumOfdov);
+    console.log(luckyNO.value);
+    if(sumOfdov === luckyNO.value){
+        outPutdiv.innerText = "you are lucky "
+       }else{
+        outPutdiv.innerText = "oh oh! y r not"
+       }
+   
+
+  
+
+}
+// function calculateSum(stringDob){
+//     stringDob = stringDob.replaceAll("-","");
+//     console.log(stringDob);
+// }
+function isLuckyNo(){
+    calculationNo()
+}
+clickBtn.addEventListener("click", isLuckyNo);
